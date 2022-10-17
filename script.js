cd25e4af90558f67370243b5bc9c4f60b227b5a5
MyatRyan <111803975+MyatRyan@users.noreply.github.com>
2022-10-17 17:06:24 -0500

    Oct 17

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('hello world');
+
+const btn = document.querySelector("#button");
+
+btn.addEventListener('click', (event) => {
+    console.log('button was click');
+});
+
+const toastTrigger = document.getElementById
+('LiveToastBtn')
+const toastLiveExample = document.getElementById
+('liveToast')
+if (toastTrigger){
+    toastTrigger.addEventListener('click'. ()=> {
+        (toastLiveExample)
+
+        toastLiveExample,show()
+    })
+}
